8f757963b99fddfdd182becc8e896f46f63d41ce
Felipe Venas <dev.felipevenas@gmail.com>
2025-07-09 11:35:43 -0300

    First commit

---
+++ src/main/java/io/github/felipevenas/api_livraria/Application.java
@@ -0,0 +1,13 @@
+package io.github.felipevenas.api_livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/felipevenas/api_livraria/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.felipevenas.api_livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
