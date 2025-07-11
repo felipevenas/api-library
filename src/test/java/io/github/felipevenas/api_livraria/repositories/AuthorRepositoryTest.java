1deb19d4ceeaf977148da037770a5e5e377c1076
Felipe Venas <dev.felipevenas@gmail.com>
2025-07-11 11:53:19 -0300

    JUNIT: Book entity, cascade operatin in book class

--- src/test/java/io/github/felipevenas/api_livraria/repositories/AuthorRepositoryTest.java
+++ src/test/java/io/github/felipevenas/api_livraria/repositories/AuthorRepositoryTest.java
@@ -1,12 +1,12 @@
 package io.github.felipevenas.api_livraria.repositories;
 
 import io.github.felipevenas.api_livraria.model.entities.Author;
+import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,9 +19,9 @@ public class AuthorRepositoryTest {
     @Test
     public void saveTest() {
        Author author = new Author();
-       author.setName("Philipe Corrêa");
-       author.setNationality("Spanish");
-       author.setDateBirthday(LocalDate.of(2001, 3, 30));
+       author.setName("Gustavo Silva");
+       author.setNationality("Brazilian");
+       author.setDateBirthday(LocalDate.of(1989, 12, 2));
 
        var savedAuthor = authorRepository.save(author);
         System.out.println("Saved author: " + savedAuthor);
@@ -29,6 +29,7 @@ public class AuthorRepositoryTest {
 
     @Test
     public void updateTest() {
+
         var id = UUID.fromString("5f627732-484a-43ad-8968-767e46f858d3");
         Optional<Author> possibleAuthor = authorRepository.findById(id);
 
@@ -44,25 +45,4 @@ public class AuthorRepositoryTest {
         }
     }
 
-    @Test
-    public void listTest() {
-        List<Author> list = authorRepository.findAll();
-        list.forEach(System.out::println);
-    }
-
-    @Test
-    public void deleteByIdTest() {
-        var id = UUID.fromString("8691ab76-c4de-4935-8243-ee19609b4f16");
-
-        Optional<Author> possibleAuthor = authorRepository.findById(id);
-        Author author = possibleAuthor.get();
-        System.out.println("Author deleted: " + author);
-        authorRepository.deleteById(id);
-    }
-
-
-    @Test
-    public void countTest() {
-        System.out.println("Quantity of authors: " + authorRepository.count());
-    }
 }
